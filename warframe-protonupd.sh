0330afd94054b9e9c60ba295d410cd9f159ecb38
elppans <elppansmk@gmail.com>
2019-08-18 22:59:57 -0300

    Update warframe-protonupd.sh

--- warframe-protonupd.sh
+++ warframe-protonupd.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-WINE="$HOME/.steam/steam/compatibilitytools.d/Proton-4.11-GE-1/dist/bin/wine64"
+#!/bin/bash
+
+# BASE
+	COMMAND=`basename "$0"`
+# BASE
+
+##	Usando arquivo de log
+
+#mkdir -p "$HOME"/."$COMMAND"
+LOGFILE="$HOME/${0##*/}".log
+
+# Habilita log copiando a saída padrão para o arquivo LOGFILE
+
+exec 1> >(tee -a "$LOGFILE")
+
+# faz o mesmo para a saída de ERROS
+
+exec 2>&1
+
+##	Usando arquivo de log - FIM
+
+PROTON="$HOME/.steam/steam/compatibilitytools.d/Proton-4.11-GE-1"
+STEAMAPPS="/mnt/files/Steam/steamapps"
+
+WINE="$PROTON/dist/bin/wine64"
 WINEARCH="win64"
 WINEDEBUG="-all"
-WINEPREFIX="/mnt/files/Steam/steamapps/compatdata/230410/pfx/"
-WINDIR="$HOME/.steam/steam/compatibilitytools.d/Proton-4.11-GE-1/dist/bin"
-WINLIB="$HOME/.steam/steam/compatibilitytools.d/Proton-4.11-GE-1/dist/lib"
-WINLIB64="$HOME/.steam/steam/compatibilitytools.d/Proton-4.11-GE-1/dist/lib64"
+WINEPREFIX="$STEAMAPPS/compatdata/230410/pfx/"
+WINDIR="$PROTON/dist/bin"
+WINLIB="$PROTON/dist/lib"
+WINLIB64="$PROTON/dist/lib64"
 
+export PROTON
+export STEAMAPPS
 export WINE
 export WINEARCH
 export WINEDEBUG
@@ -15,5 +41,5 @@ export WINEPREFIX
 export PATH="$WINDIR:$PATH"
 export LD_LIBRARY_PATH="$WINLIB:$WINLIB64:$LD_LIBRARY_PATH"
 
-cd /mnt/files/Steam/steamapps/common/Warframe/Tools/
+cd "$STEAMAPPS/common/Warframe/Tools/"
 ./updater.sh --no-game -v
