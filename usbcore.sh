be401e202241d70f986cceacacb9e19b7789c591
elppans <elppansmk@gmail.com>
2019-10-05 10:15:53 -0300

    Add files via upload

---
+++ usbcore.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "-1" | tee /sys/module/usbcore/parameters/autosuspend
+echo "-1" | tee /sys/bus/usb/devices/usb*/power/autosuspend
+echo "-1" | tee /sys/bus/usb/devices/usb*/power/autosuspend_delay_ms
+echo "on" | tee /sys/bus/usb/devices/usb*/power/control
+
+sed -i "s/exit 0//g" /etc/rc.local
+echo -e '
+echo "-1" | tee /sys/module/usbcore/parameters/autosuspend
+echo "-1" | tee /sys/bus/usb/devices/usb*/power/autosuspend
+echo "-1" | tee /sys/bus/usb/devices/usb*/power/autosuspend_delay_ms
+echo "on" | tee /sys/bus/usb/devices/usb*/power/control
+\n\nexit 0
+' | sudo tee -a /etc/rc.local
+
+echo "options usbcore autosuspend=-1" | sudo tee /etc/modprobe.d/disable-usb-autosuspend.conf > /dev/null
+
+sed -i "/GRUB_CMDLINE_LINUX_DEFAULT/s/quiet/quiet usbcore.autosuspend=-1/g" /etc/default/grub
+update-grub
+
+# Fontes:
+#
+# https://askubuntu.com/questions/1044988/usb-ports-not-working-after-resume-from-sleep-on-ubuntu-18-04
+# https://unix.stackexchange.com/questions/91027/how-to-disable-usb-autosuspend-on-kernel-3-7-10-or-above
+# https://sobrelinux.info/questions/771630/how-to-disable-usb-autosuspend-on-kernel-3-7-10-or-above
+# https://ubuntuforums.org/archive/index.php/t-1968487.html
+# https://kodi.wiki/view/HOW-TO:Suspend_and_wake_in_Ubuntu
+# https://bbs.archlinux.org/viewtopic.php?id=199504
+
