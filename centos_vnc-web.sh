6c7c7042686d720accfadd0790926039e2e7b383
elppans <elppansmk@gmail.com>
2019-10-26 08:30:16 -0300

    Update centos_vnc-web.sh

--- centos_vnc-web.sh
+++ centos_vnc-web.sh
@@ -13,9 +13,10 @@ cd /usr/share/kanaka-noVNC/utils/
 ./launch.sh "\$@"
 EOF
 chmod +x /usr/bin/novnc
-mkdir -p /usr/share/kanaka-noVNC/web
-cp -rfv /usr/share/kanaka-noVNC/vnc*.html /usr/share/kanaka-noVNC/web/
+#mkdir -p /usr/share/kanaka-noVNC/web
+#cp -rfv /usr/share/kanaka-noVNC/vnc*.html /usr/share/kanaka-noVNC/web/
 #/usr/bin/novnc --web /usr/share/kanaka-noVNC/web/
+ln -sf /usr/share/kanaka-noVNC/vnc.html /usr/share/kanaka-noVNC/index.html
 
 yum -y install x11vnc
 x11vnc -storepasswd
@@ -23,7 +24,7 @@ x11vnc -storepasswd
 cat >/usr/bin/x11vnc-browser<<EOF
 #!/bin/bash
 x11vnc -forever -auth guess -cursor arrow -shared -rfbport 5900 -rfbauth /root/.vnc/passwd -noxdamage 2>&1 1>& /var/log/x11vnc.log &
-/usr/bin/novnc --listen 6080 --vnc localhost:5900 --web /usr/share/kanaka-noVNC/web/ 2>&1 1>& /var/log/noVNC.log &
+/usr/bin/novnc --listen 6080 --vnc localhost:5900 --web /usr/share/kanaka-noVNC/ 2>&1 1>& /var/log/noVNC.log &
 iptables -I INPUT -p tcp --dport 6080 -j ACCEPT
 iptables -nL |grep 6080
 EOF
@@ -46,9 +47,10 @@ EOF
 systemctl daemon-reload
 systemctl start x11vnc-browser.service
 systemctl enable x11vnc-browser.service
+systemctl status x11vnc-browser.service
 
 #
 
 # Backup:
 cd /opt
-tar -zcvf kanaka-novnc_git-`date +%d%m%y`.tgz /usr/share/kanaka-noVNC /usr/bin/novnc /usr/bin/x11vnc-browser /etc/systemd/system/x11vnc-browser.service
+tar -zcf kanaka-novnc_git-`date +%d%m%y`.tgz /usr/share/kanaka-noVNC /usr/bin/novnc /usr/bin/x11vnc-browser /etc/systemd/system/x11vnc-browser.service
