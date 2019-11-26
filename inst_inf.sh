766d19d4760687150dd3035d37dd95c6b8cda0d2
elppans <elppansmk@gmail.com>
2019-11-26 08:21:00 -0300

    Update inst_inf.sh

--- inst_inf.sh
+++ inst_inf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+# Fonte:
+# https://www.mariolb.com.br/blog/2014/05/30/como_saber_quando_um_sistema_linux_foi_instalado.html
 dumpe2fs $(mount | grep "on \/ ") | grep -i "Filesystem created"
