3fc0abcb4cc2393e4d72a9aae1dc385b315a778b
elppans <elppansmk@gmail.com>
2020-09-18 12:22:53 -0300

    Update usbcore.sh

--- usbcore.sh
+++ usbcore.sh
@@ -6,6 +6,7 @@ echo "-1" | tee /sys/bus/usb/devices/usb*/power/autosuspend_delay_ms
 echo "on" | tee /sys/bus/usb/devices/usb*/power/control
 
 sed -i "s/exit 0//g" /etc/rc.local
+sed -i "s/.\/startup &//g" /etc/rc.local
 echo -e '
   echo "usbcore on"
 echo "-1" | tee /sys/module/usbcore/parameters/autosuspend >> /dev/null
@@ -14,7 +15,7 @@ echo "-1" | tee /sys/bus/usb/devices/usb*/power/autosuspend_delay_ms >> /dev/nul
 echo "on" | tee /sys/bus/usb/devices/usb*/power/control >> /dev/null
 \n\nexit 0
 ' | sudo tee -a /etc/rc.local
-
+sed -i '/exit/i ./startup &'  /etc/rc.local
 echo "options usbcore autosuspend=-1" | sudo tee /etc/modprobe.d/disable-usb-autosuspend.conf > /dev/null
 
 sed -i "/GRUB_CMDLINE_LINUX_DEFAULT/s/quiet/quiet net.ifnames=0 biosdevname=0 usbcore.autosuspend=-1/g" /etc/default/grub
