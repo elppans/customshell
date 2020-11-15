d9cbda525bb3e2930c2c0a47536cc0868107603a
elppans <elppansmk@gmail.com>
2020-11-15 00:18:06 -0300

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # BASE
-	COMMAND=`basename "$0"`
+        COMMAND=`basename "$0"`
 # BASE
 
-##	Usando arquivo de log
+##      Usando arquivo de log
 
 #mkdir -p "$HOME"/."$COMMAND"
 LOGFILE="$HOME/${0##*/}".log
-
+LOGFILEERROR="$HOME/${0##*/}"_error.log
 # Habilita log copiando a saída padrão para o arquivo LOGFILE
 
 exec 1> >(tee -a "$LOGFILE")
 
 # faz o mesmo para a saída de ERROS
 
-exec 2>&1
+exec 2> >(tee -a "$LOGFILEERROR")
 
-##	Usando arquivo de log - FIM
-
+##      Usando arquivo de log - FIM
