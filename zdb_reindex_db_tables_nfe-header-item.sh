bc61a03ad465bcd54b8d544c67a2a38911f9eed0
elppans <elppansmk@gmail.com>
2022-06-24 14:54:35 -0300

    Add files via upload

---
+++ zdb_reindex_db_tables_nfe-header-item.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+echo "Deve executar o comando como super usuario!"
+exit 0
+fi
+
+##      Usando arquivo de log - INÍCIO
+
+# Habilita log copiando a saída padrão para o arquivo LOGFILE
+
+exec 1> >(tee -a "/var/log/"${0##*/}".log")
+
+# faz o mesmo para a saída de ERROS
+
+exec 2> >(tee -a "/var/log/"${0##*/}"_error.log")
+
+##      Usando arquivo de log - FIM
+
+#tab_controle_nfe
+#tab_nota_header
+#tab_nota_item
+
+IP="127.0.0.1"
+PORT="5432"
+BANCO="ZeusRetail"
+
+ echo -e "INICIO $(date)"
+echo "public.tab_controle_nfe"
+psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "REINDEX TABLE public.tab_controle_nfe;"
+echo "public.tab_nota_header"
+psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "REINDEX TABLE public.tab_nota_header;"
+echo "public.tab_nota_item"
+psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "REINDEX TABLE public.tab_nota_item;"
+ echo -e "FIM $(date)"
