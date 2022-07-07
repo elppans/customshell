4781e99a05d4e40b56de42c12757862faa4c5bec
elppans <elppansmk@gmail.com>
2022-07-07 15:45:13 -0300

    Update zdb_vacuum_analyze_db_tables_cnfe-nheader-nitem.sh

--- zdb_vacuum_analyze_db_tables_cnfe-nheader-nitem.sh
+++ zdb_vacuum_analyze_db_tables_cnfe-nheader-nitem.sh
@@ -26,7 +26,62 @@ PORT="5432"
 BANCO="ZeusRetail"
 
  echo -e "INICIO $(date)"
-psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "VACUUM FULL VERBOSE ANALYZE public.tab_controle_nfe;"
-psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "VACUUM FULL VERBOSE ANALYZE public.tab_nota_header;"
-psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "VACUUM FULL VERBOSE ANALYZE public.tab_nota_item;"
+
+# Via psql, VACUUM e ANALYZE em tabela determinada:
+
+#psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "VACUUM FULL VERBOSE ANALYZE public.tab_controle_nfe;"
+#psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "VACUUM FULL VERBOSE ANALYZE public.tab_nota_header;"
+#psql -h "$IP" -p "$PORT" -U postgres -d "$BANCO" -c "VACUUM FULL VERBOSE ANALYZE public.tab_nota_item;"
+
+# Via vacuumdb, VACUUM e posteriormente ANALYZE em tabela determinada:
+
+#vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -f -t public.tab_controle_nfe 1>/var/log/vacuumdb.tab_controle_nfe.1.log 2>/var/log/vacuumdb.tab_controle_nfe.2.log
+#vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -Z -t public.tab_controle_nfe 1>/var/log/vacuumdb_analyze-only.tab_controle_nfe.1.log 2>/var/log/vacuumdb_analyze-only.tab_controle_nfe.2.log
+
+#vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -f -t public.tab_nota_header 1>/var/log/vacuumdb.tab_nota_header.1.log 2>/var/log/vacuumdb.tab_nota_header.2.log
+#vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -Z -t public.tab_nota_header 1>/var/log/vacuumdb_analyze-only.tab_nota_header.1.log 2>/var/log/vacuumdb_analyze-only.tab_nota_header.2.log
+
+#vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -f -t public.tab_nota_item 1>/var/log/vacuumdb.tab_nota_item.1.log 2>/var/log/vacuumdb.tab_nota_item.2.log
+#vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -Z -t public.tab_nota_item 1>/var/log/vacuumdb_analyze-only.tab_nota_item.1.log 2>/var/log/vacuumdb_analyze-only.tab_nota_item.2.log
+
+# Via vacuumdb, VACUUM e ANALYZE em tabela determinada:
+
+vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -f -z -t public.tab_controle_nfe 1>/var/log/vacuumdb.tab_controle_nfe.1.log 2>/var/log/vacuumdb.tab_controle_nfe.2.log
+vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -f -z -t public.tab_nota_header 1>/var/log/vacuumdb.tab_nota_header.1.log 2>/var/log/vacuumdb.tab_nota_header.2.log
+vacuumdb -h "$IP" -p "$PORT" -U postgres -w -d "$BANCO" -v -f -z -t public.tab_nota_item 1>/var/log/vacuumdb.tab_nota_item.1.log 2>/var/log/vacuumdb.tab_nota_item.2.log
+
  echo -e "FIM $(date)"
+
+### Sobre o VACUUMDB:
+#
+#vacuumdb cleans and analyzes a PostgreSQL database.
+#
+#Usage:
+#  vacuumdb [OPTION]... [DBNAME]
+#
+#Usage:
+#  vacuumdb [OPTION]... [DBNAME]
+#
+#Options:
+#  -a, --all                       vacuum all databases
+#  -d, --dbname=DBNAME             database to vacuum
+#  -e, --echo                      show the commands being sent to the server
+#  -f, --full                      do full vacuuming
+#  -F, --freeze                    freeze row transaction information
+#  -q, --quiet                     don't write any messages
+#  -t, --table='TABLE[(COLUMNS)]'  vacuum specific table(s) only
+#  -v, --verbose                   write a lot of output
+#  -V, --version                   output version information, then exit
+#  -z, --analyze                   update optimizer statistics
+#  -Z, --analyze-only              only update optimizer statistics
+#      --analyze-in-stages         only update optimizer statistics, in multiple
+#                                  stages for faster results
+#  -?, --help                      show this help, then exit
+#
+#Connection options:
+#  -h, --host=HOSTNAME       database server host or socket directory
+#  -p, --port=PORT           database server port
+#  -U, --username=USERNAME   user name to connect as
+#  -w, --no-password         never prompt for password
+#  -W, --password            force password prompt
+#  --maintenance-db=DBNAME   alternate maintenance database
