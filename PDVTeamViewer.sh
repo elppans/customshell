891c63777a4909040f67bbdc0108bbda95987ba8
elppans <elppansmk@gmail.com>
2023-01-26 13:31:49 -0300

    Update PDVTeamViewer.sh

--- PDVTeamViewer.sh
+++ PDVTeamViewer.sh
@@ -2,28 +2,8 @@
 
 # Instalando Team Viwer no PDV
 
-#Team Viewer EXIGE Interface gráfica E login via GUI.
-#Então DEVE ser instalado novamente o GUI padrão do PDV, desabilitar o autologin via getty e habilitar autologin via GUI
-
-# Instalando e configurando GUI padrão do PDV (LXDM)
-
-sudo apt-get update
-sudo apt-get -y install lxdm
-sudo sed -i '/autologin/s/dgod/root/' /etc/lxdm/default.conf
-sudo sed -i '/autologin/s/#//' /etc/lxdm/default.conf
-sed -i 's/ExecStart=-/#ExecStart=-/' /etc/systemd/system/getty@tty1.service.d/override.conf
-
-## Ps.: Se fizer alguma configuração ruim na barra (painel) do LXDE, resete com os seguintes comandos:
-
-# rm -rf ~/.config/lxpanel
-# cp -rf /etc/xdg/lxpanel ~/.config/
-# lxpanelctl restart
-
-## Após iniciar o sistema, configurar para ocultar a barra de tarefas
-
-# Clique com o botão direito em cima da barra de tarefas (painel)  e clique em "Painel Settings"
-# Vá até a aba "Advanced", procure "Automatic hiding" e clique para deixar marcado a opção "Minimize panel when not in use"
-# Clique em Close
+# Team Viewer EXIGE Interface gráfica E login via GUI.
+# Então DEVE ser instalado novamente o GUI padrão do PDV, desabilitar o autologin via getty e habilitar autologin via GUI
 
 # Instalando e configurando Team Viewer 
 
@@ -46,6 +26,26 @@ Icon=TeamViewer
 ' | tee ~/Desktop/TeamViewer.desktop
 chmod +x ~/Desktop/TeamViewer.desktop
 cp -v ~/Desktop/TeamViewer.desktop ~/.config/autostart 
+
+# Instalando e configurando GUI padrão do PDV (LXDM)
+
+sudo apt-get update
+sudo apt-get -y install lxdm
+sudo sed -i '/autologin/s/dgod/root/' /etc/lxdm/default.conf
+sudo sed -i '/autologin/s/#//' /etc/lxdm/default.conf
+sed -i 's/ExecStart=-/#ExecStart=-/' /etc/systemd/system/getty@tty1.service.d/override.conf
+
+## Ps.: Se fizer alguma configuração ruim na barra (painel) do LXDE, resete com os seguintes comandos:
+
+# rm -rf ~/.config/lxpanel
+# cp -rf /etc/xdg/lxpanel ~/.config/
+# lxpanelctl restart
+
+## Após iniciar o sistema, configurar para ocultar a barra de tarefas
+
+# Clique com o botão direito em cima da barra de tarefas (painel)  e clique em "Painel Settings"
+# Vá até a aba "Advanced", procure "Automatic hiding" e clique para deixar marcado a opção "Minimize panel when not in use"
+# Clique em Close
 
 reboot
 
