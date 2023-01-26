804572484237d761ed91655716e5371a0eb93641
elppans <elppansmk@gmail.com>
2023-01-26 09:21:01 -0300

    Update PDVTeamViewer.sh

--- PDVTeamViewer.sh
+++ PDVTeamViewer.sh
@@ -6,13 +6,21 @@
 #Então DEVE ser instalado novamente o GUI padrão do PDV, desabilitar o autologin via getty e habilitar autologin via GUI
 
 # Instalando e configurando GUI padrão do PDV (LXDM)
+
 sudo apt-get update
 sudo apt-get -y install lxdm
 sudo sed -i '/autologin/s/dgod/root/' /etc/lxdm/default.conf
 sudo sed -i '/autologin/s/#//' /etc/lxdm/default.conf
 sed -i 's/ExecStart=-/#ExecStart=-/' /etc/systemd/system/getty@tty1.service.d/override.conf
 
+## Ps.: Se fizer alguma configuração ruim na barra (painel) do LXDE, resete com os seguintes comandos:
+
+# rm -rf ~/.config/lxpanel
+# cp -rf /etc/xdg/lxpanel ~/.config/
+# lxpanelctl restart
+
 # Instalando e configurando Team Viewer 
+
 sudo sh -c "echo 'deb http://linux.teamviewer.com/deb stable main' >> /etc/apt/sources.list.d/teamviewer.list"
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C5E224500C1289C0
 sudo apt-get update
