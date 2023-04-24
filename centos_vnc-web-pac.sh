b2250e005bd7374818e313c76fd3fc46e69d6ce6
elppans <elppansmk@gmail.com>
2023-04-24 10:04:48 -0300

    Update centos_vnc-web-pac.sh

--- centos_vnc-web-pac.sh
+++ centos_vnc-web-pac.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #https://kanaka.github.io/noVNC/
-wget -c https://www.dropbox.com/s/ta0xtm5rnyzjznu/kanaka-novnc_git-040220.tgz
+#wget -c https://www.dropbox.com/s/ta0xtm5rnyzjznu/kanaka-novnc_git-040220.tgz
+wget -c https://github.com/elppans/conf/raw/master/novnc_web/kanaka-novnc_git-040220.tgz
 tar -zxvf kanaka-novnc_git-040220.tgz -C /
 
 yum updateinfo
