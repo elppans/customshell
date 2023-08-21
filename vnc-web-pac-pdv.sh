b6fd433453fe9b29e1b86d3092699ea89df514cd
elppans <elppansmk@gmail.com>
2023-08-21 16:38:13 -0300

    Update vnc-web-pac-pdv.sh

--- vnc-web-pac-pdv.sh
+++ vnc-web-pac-pdv.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-#https://kanaka.github.io/noVNC/
-# wget -c https://www.dropbox.com/s/ta0xtm5rnyzjznu/kanaka-novnc_git-040220.tgz
-wget -c https://github.com/elppans/conf/raw/master/novnc_web/kanaka-novnc_git-040220.tgz
-tar -zxvf kanaka-novnc_git-040220.tgz -C /
+# https://kanaka.github.io/noVNC/
 
+# Baixar o pacote noVNC
+wget -c https://github.com/elppans/conf/raw/master/novnc_web/kanaka-novnc_git-210823.tgz
+tar -zxvf kanaka-novnc_git-210823.tgz -C /
+
+# Desativar o noVNC do PDV
+chmod -x /usr/local/bin/noVNC
+sed -i '/[Vv][Nn][Cc]/ s/^/# /' /usr/local/bin/display.set
+# mv /opt/webadmin/thirdparty/kanaka-noVNC /opt/webadmin/thirdparty/kanaka-noVNC.OLD
+
+# Instalar x11vnc e adicionar a senha
 sudo apt-get update
-sudo apt-get install x11vnc
+sudo apt-get -y install x11vnc
 x11vnc -storepasswd
 
+# Ativar serviço
 systemctl daemon-reload
 systemctl start x11vnc-browser.service
 systemctl enable x11vnc-browser.service
