d5639d2532d4f14bd13ae2230759e9275f1b9446
elppans <elppansmk@gmail.com>
2023-09-24 20:00:22 -0300

    Create wbvsshclient_arch-inst.sh

---
+++ wbvsshclient_arch-inst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pacman -Syu --needed --noconfirm wine-mono
+DGET="~/Downloads/bitvise" && mkdir -p $DGET
+export WINEPREFIX="$HOME/.wine/wbvsshclient" && mkdir -p $WINEPREFIX
+export WINEDEBUG="-all"
+cd "$DGET"
+wget -c https://dl.bitvise.com/BvSshClient-Inst.exe
+wine "BvSshClient-Inst.exe -force -acceptEULA"  "$@"
+cd
+rm -rf "$DGET"
